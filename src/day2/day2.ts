171a7ea50daad24d5634cc67c63f699a46f889e9
Fisher Bissett <southpawfish@gmail.com>
2021-12-02 00:19:42 -0600

    day 2

--- src/day2/day2.ts
+++ src/day2/day2.ts
@@ -1,12 +1,41 @@
 import { readFileSync } from 'fs';
+
+const directions: string[] = readFileSync(__dirname + '/day2.input.txt')
+  .toString()
+  .split('\n')
+  .map((item) => item);
 //
 // ----------------------puzzle one----------------------------
 //
-const puzzleOne = () => {};
+const puzzleOne = () => {
+  let position = 0;
+  let depth = 0;
+  for (let i = 0; i < directions.length; i++) {
+    let currentValue = Number(directions[i].charAt(directions[i].length - 1));
+    if (directions[i].includes('down')) depth = depth + currentValue;
+    if (directions[i].includes('up')) depth = depth - currentValue;
+    if (directions[i].includes('forward')) position = position + currentValue;
+  }
+  return position * depth;
+};
 //
 // ----------------------puzzle two----------------------------
 //
-const puzzleTwo = () => {};
+const puzzleTwo = () => {
+  let position = 0;
+  let depth = 0;
+  let aim = 0;
+  for (let i = 0; i < directions.length; i++) {
+    let currentValue = Number(directions[i].charAt(directions[i].length - 1));
+    if (directions[i].includes('down')) aim = aim + currentValue;
+    if (directions[i].includes('up')) aim = aim - currentValue;
+    if (directions[i].includes('forward')) {
+      position = position + currentValue;
+      depth = depth + aim * currentValue;
+    }
+  }
+  return position * depth;
+};
 
 exports.puzzleOne = puzzleOne;
 exports.puzzleTwo = puzzleTwo;
